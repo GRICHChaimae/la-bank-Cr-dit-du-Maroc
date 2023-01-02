8c0fdd0b3af3460a916ca8b428f183d176b3be04
GRICHChaimae <93976924+GRICHChaimae@users.noreply.github.com>
2023-01-02 11:20:09 +0100

    Connect with Mongoose

--- API/server.js
+++ API/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 require('dotenv').config()
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const colors = require('colors')
+
+connectDB()
 
 const app = express()
 
-app.listen(port, console.log(`Server runing on port ${port}`))
+
+
+app.listen(port, console.log(`Server runing on port ${port}`)) 
