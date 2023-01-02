a722f77a25c2ba68249af020c0b7d878e9ffce86
GRICHChaimae <93976924+GRICHChaimae@users.noreply.github.com>
2023-01-02 11:52:01 +0100

    create user schema

---
+++ API/users/models/authModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema(
+    {
+        fname: {
+            type: String,
+            required: true
+        },
+        lname: {
+            type: String,
+            required: true
+        },
+        CIN: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('user', userSchema)
